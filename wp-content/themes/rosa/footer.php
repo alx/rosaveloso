95b79bdc5935bc78002c62a11d24de5c9cb095b9
Alexandre Girard <alx.girard@gmail.com>
2009-08-20 13:13:16 +0200

    remove wordpress credit

--- wp-content/themes/rosa/footer.php
+++ wp-content/themes/rosa/footer.php
@@ -7,8 +7,6 @@
 <!-- begin footer -->
 </div>
 
-<p class="credit"><!--<?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. --> <cite><?php echo sprintf(__("Powered by <a href='http://wordpress.org/' title='%s'><strong>WordPress</strong></a>"), __("Powered by WordPress, state-of-the-art semantic personal publishing platform.")); ?></cite></p>
-
 <?php wp_footer(); ?>
 </body>
 </html>
